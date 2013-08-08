d37c41d96d543e6aca6eb2716e348174672350f4
Bin Wang <bwang@southalabama.edu>
2013-08-08 00:00:00 +0000

    version 1.13-8-8

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-.onLoad <- function(lib, pkg){
-  packageStartupMessage("spt 1.1 loaded ...")
-  assign('.sptConnect',NULL,pos=.GlobalEnv) 
-}
-
 .onUnload <- function(libpath)
     library.dynam.unload("spt",  libpath)
 
